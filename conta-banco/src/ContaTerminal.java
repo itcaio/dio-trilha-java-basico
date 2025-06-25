796f9ea337cc1adfca79fc372ee0d6fb84ac71ac
itcaio <italocaio.oficial@gmail.com>
2025-06-24 22:15:20 -0300

    CONHECENDO O COMENTÁRIO ESPECIAL 'TODO:'

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,7 +1,13 @@
 
 public class ContaTerminal {
     public static void main(String[] args) {
+        //Todo: Conhecer e importar a class Scanner
 
+
+        //TODO:EXIBIR AS MENSAGENS PARA O USUÁRIO
+
+        //TODO: OBTER PELAS Scanners os valores digitados no terminal
+
+        //TODO:Exibir a mensagem de conta criada
         }
     }
-}
