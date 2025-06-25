ecfcd6050f41393eabe94a03f6d8f1bae728e195
itcaio <italocaio.oficial@gmail.com>
2025-06-25 19:09:52 -0300

    FINALIZANDO O PROJETO

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        //Todo: Conhecer e importar a class Scanner
+      Scanner scanner = new Scanner(System.in);
+
+        System.out.println("-----------------------------------------");
+        System.out.println("    -Seja Bem Vindo ao Nosso Banco! ;) \n -Para abrir sua conta, siga os passos a seguir: ");
+        System.out.println(" -Nos diga seu nome completo: ");
+        String nome = scanner.nextLine();
+        System.out.printf(" -Ok, %s! Agora crie um número de 4 dígitos para sua conta: \n", nome);
+        int nDaConta = scanner.nextInt();
+        System.out.printf(" -conta %s criada com sucesso! \n -Agora crie um numero de agencia + digito: [ex: 123-4]\n", nDaConta);
+        String agencia = scanner.next();
+        System.out.printf(" -Agência %s criada com sucesso! \n -Agora digite o seu Saldo Bancário:\n ", agencia);
+        double saldo = scanner.nextDouble();
+        System.out.println(" -Tudo certo! Obrigado por abrir sua conta em nosso Banco!");
+        System.out.printf(Locale.forLanguageTag("pt-BR"), "-SEGUE SEUS DADOS: \n NOME: %s \n NÚMERO DA CONTA: %s\n AGÊNCIA: %S\n SALDO: R$ %.2f%n \n ", nome, nDaConta, agencia, saldo);
 
 
-        //TODO:EXIBIR AS MENSAGENS PARA O USUÁRIO
-
-        //TODO: OBTER PELAS Scanners os valores digitados no terminal
-
-        //TODO:Exibir a mensagem de conta criada
-        }
+    }
     }
